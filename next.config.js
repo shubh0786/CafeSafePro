58acee1080f6e612300f4907dcd7cdb69315efe7
SHUBHAM SINGH <shubham.singh267@gmail.com>
2026-02-26 01:32:47 +1300

    Add webpack alias configuration to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 const nextConfig = {
   images: {
     domains: ['localhost'],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.join(__dirname),
+    }
+    return config
   },
 }
 
